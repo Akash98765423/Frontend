312ddda0f7ac2228d3fdd61ec12e2c04fb1ad19d
Akash <aakash59918@gmail.com>
2026-08-20 20:29:30 +0530

    Completed

---
+++ JAVASCRIPT/day4/assets/task3.js
@@ -0,0 +1,13 @@
+let mark = 85;
+
+if (mark >= 90) {
+    console.log("A+");
+} else if(mark >= 89){
+    console.log("A");
+}else if(mark > 74) {
+    console.log("B");
+}else if(mark >= 49) {
+    console.log("C");
+}else{
+    console.log("Fail");
+}
---
+++ JAVASCRIPT/day4/assets/task4.js
@@ -0,0 +1,8 @@
+let userName = "Akash";
+let passWord = "1234"
+
+if(userName==="Akash" && passWord === "1234"){
+    console.log("Login Success");
+}else{
+    console.log("Invalid Login");
+}   
