2ed25221860d45351ccc6e5482bcaeb940928542
Akash <aakash59918@gmail.com>
2026-09-03 23:38:41 +0530

    Completed

---
+++ JAVASCRIPT/day11/assets/task4.js
@@ -0,0 +1,12 @@
+const arr = ["Akash","vicky","Tamil"]
+
+arr.unshift("bharathi")
+arr.unshift("abi")
+
+console.log(arr);
+
+arr.shift()
+
+console.log(arr);
+
+
---
+++ JAVASCRIPT/day11/assets/task5.js
@@ -0,0 +1,14 @@
+const arr = [10,20,30]
+
+const newarr = []
+
+for (let a = 0; a < arr.length;a++) {
+
+    newarr[a] = arr[a]
+}
+
+newarr[3]= 40
+
+console.log(newarr);
+
+
